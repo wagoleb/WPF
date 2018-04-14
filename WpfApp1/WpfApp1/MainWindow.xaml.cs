50801128d2c6a2f14a416288bcd5909f3ff4c2b9
bonowg <bonowg@gmail.com>
2018-04-14 15:51:13 +0200

    sekcja-9_lekcja-52-scroll-viewer

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,58 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            piosenka.Text = @"[Hook]
+My name is Kodak but you know that already I don't want the wap baby I just want the fetty Polo to the socks homie this ain't Perry Ellis Let me get the raw I'm a ball like spaghetti
+It's getting hot in here the block be hot no not Nelly You can keep the Rari, I'd rather have a Chevy I don't want no head lately I've been getting becky I just want the bread, keep the peanut butter jelly
+
+[Verse 1]
+She walked up to me, I was by the bar, she was like hello
+She rolled up on me, I was rollin up my cigarillo
+No I don't fuck with Grabba leaf I'm breaking down the elpo
+I'm always smoking loud I can't keep it on the LO
+Girl I don't like the way your booty flop, it's like jello
+And now I'm rockin Robin Jeans remember wearing Ecko
+She say she just came to chill look bitch this ain't a Getty
+I told that bitch get outta here, act like you never met me
+I'm up the road I'm lurking for them books, no library
+Lil niggas state - to - state I get more cake than lil Debbie
+No I can't save a hoe I'm no neighborhood hero
+Keep that shit 1K I can't be fuckin wit a zero
+
+[Hook]
+My name is Kodak but you know that already
+I don't want the wap baby I just want the fetty
+Polo to the socks homie this ain't Perry Ellis
+Let me get the raw I'mma ball like spaghetti
+It's getting hot in here the block be hot no not Nelly
+You can keep the Rari, I'd rather have a Chevy
+I don't want no head lately I've been getting becky
+I just want the bread, keep the peanut butter jelly
+
+
+[Verse 2]
+Boy I need that green bought my green no asparagus
+Lord forgive me please cut the money I just cherished it
+Hit a mean lick I took that money and I buried it
+She get on her knees OMG that's embarassing
+I don't need no record deal but you gon hear my record still
+Me and DJ on the pill we spinning like a ferris wheel
+Oh that's Kodak Black who they be talking bout,that's the nigga
+First I had to grow to be the man like a caterpillar
+Once that jit was old enough to thug it he was thuggin
+He just found a strap now he cut you like it's nothing
+I use to pour fours now I pour a whole dozen
+Bumped into Lil Wally in the hood, hey cousin
+
+[Hook]
+My name is Kodak but you know that already
+I don't want the wap baby I just want the fetty
+Polo to the socks homie this ain't Perry Ellis
+Let me get the raw I'mma ball like spaghetti
+It's getting hot in here the block be hot no not Nelly
+You can keep the Rari, I'd rather have a Chevy
+I don't want no head lately I've been getting becky
+I just want the bread, keep the peanut butter jelly ";
         }
     }
 }
