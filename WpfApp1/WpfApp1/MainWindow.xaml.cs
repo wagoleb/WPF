354610868ef8e11a9e4ac53f280a897f24cb7282
bonowg <bonowg@gmail.com>
2018-04-14 14:57:50 +0200

    sekcja-9_lekcja-48

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            siatka.Columns = Convert.ToInt32(kolumny.Text);
+        }
     }
 }
