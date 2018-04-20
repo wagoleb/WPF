5d33e42d9982d53dc3532b7cebdc3e309610b48c
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-20 14:57:48 +0200

    date triiggers

---
+++ WpfApp1/WpfApp1/MyImages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyImages
+{
+    class MyImages
+    {
+        public string pathToImage { get; set; }
+        public string nameOfImage { get; set; }
+        public bool isFound { get; set; }
+
+        public MyImages()
+        {
+            this.pathToImage = @"C:\repos\WPF\WpfApp1\WpfApp1\ubuntu.jpg";
+            this.nameOfImage = "UBUNTU";
+        }
+    }
+}
