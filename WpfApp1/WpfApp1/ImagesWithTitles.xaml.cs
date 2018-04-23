86bd51d40f1dc4188449685ee38faf587eb27740
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-23 12:03:05 +0200

    UserControl

---
+++ WpfApp1/WpfApp1/ImagesWithTitles.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for ImagesWithTitles.xaml
+    /// </summary>
+    public partial class ImagesWithTitles : UserControl
+    {
+        private double _szerokosc;
+        public double szerokosc
+        {
+            get
+            {
+                return _szerokosc;
+            }
+            set
+            {
+                this._szerokosc = value;
+                this.Width = this.szerokosc / 4;
+            }
+        }
+        public ImagesWithTitles()
+        {
+            InitializeComponent();
+        }
+    }
+}
