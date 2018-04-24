c007ec6f6a383d28b46e6789bb24cb5501bf6979
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-24 11:16:00 +0200

    ListBox add and remove

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -17,7 +17,20 @@ namespace WpfApp1
 
         private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            MessageBox.Show(ListaImion.SelectedIndex.ToString());
+            // MessageBox.Show(ListaImion.SelectedItems.ToString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)noweImie.Text != "")
+                ListaImion.Items.Add((string)noweImie.Text);
+            noweImie.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (ListaImion.SelectedIndex > 0)
+                ListaImion.Items.RemoveAt(ListaImion.SelectedIndex);
         }
     }
 }
