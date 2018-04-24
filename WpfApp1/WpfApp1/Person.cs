af0d29d52046d5942f713bc54b026a6ddac5df5b
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-24 10:21:47 +0200

    contentTemplate, contentPresenter, DataTemplate

---
+++ WpfApp1/WpfApp1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonNS
+{
+    class Person
+    {
+        public string Sex { get; set; }
+        public string Name { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
