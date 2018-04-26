112b547f8e853fb2bf4ff25b32a5bd6658b2e91b
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-26 10:44:35 +0200

    TemplateSelector

---
+++ WpfApp1/WpfApp1/GenderTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using ListBoxControl;
+
+namespace GenderTemplateSelectorNM
+{
+    class GenderTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MaleTemplate { get; set; }
+        public DataTemplate FemaleTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            PersonData person = item as PersonData;
+
+            if (person.genderType == PersonData.gender.male)
+                return MaleTemplate;
+
+            return FemaleTemplate;
+
+        }
+    }
+}
