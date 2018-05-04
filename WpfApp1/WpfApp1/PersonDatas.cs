457f5f2eb32288608202fedf3bce831be85cd378
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-05-04 13:47:10 +0200

    multibinding string format

---
+++ WpfApp1/WpfApp1/PersonDatas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiBindingNS
+{
+    class PersonDatas
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
